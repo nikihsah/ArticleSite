565e3b1a7681958726c19c73830378798ae3a575
nikihsah <nikihsah@gmail.com>
2022-02-02 00:29:49 +0300

    - Added composer var_dump.

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
+//composer ^_^
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 define('ROOT', dirname(__FILE__));
 require_once(ROOT . '/Components/Router.php');
+require __DIR__ . '/vendor/autoload.php';
 
 //4 Вызов Route
 $route = new Router();
+dd('asdad');
 $route->run();
